d92ded778b12cfab6b600d8fe763965e9237351d
Ramesh Babu <consort23@gmail.com>
2024-03-05 16:09:07 +0530

    Committing frontend.sh for changes

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,19 @@
-dnf install nginx -y
-systemctl enable nginx
-systemctl start nginx
+source common.sh
 
-cp expense.conf /etc/nginx/default.d/expense.conf
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
+app_dir=/usr/share/nginx/html
+component=frontend
+
+Print_Task_Heading "Install Nginx"
+dnf install nginx -y &>>$LOG
+Check_Status $?
+
+Print_Task_Heading "Copy Expense Nginx Configuration"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
+Check_Status $?
+
+App_PreReq
+
+Print_Task_Heading "Start Nginx Service"
+systemctl enable nginx &>>$LOG
+systemctl restart nginx &>>$LOG
+Check_Status $?
